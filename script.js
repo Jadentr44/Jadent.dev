acaa9a64d3c2b4a736ca0b88fa91fe02c2fd15da
Jaden <jadentr.44@gmail.com>
2023-05-15 22:27:35 -0500

    ui bugs

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-document.getElementById("toggleMenu").addEventListener('click',function(){
-if(document.getElementById("list").classList.contains("hidden")){
-  document.getElementById("list").classList.remove("hidden")
-}else{
-  document.getElementById("list").classList.add("hidden")
+document.querySelectorAll(".toggleMenu").forEach(el=>{
 
-}
+  el.addEventListener('click',function(){
+    if(document.getElementById("list").classList.contains("hidden")){
+      document.getElementById("list").classList.remove("hidden")
+    }else{
+      document.getElementById("list").classList.add("hidden")
+    
+    }
+    })
 })
+
+
